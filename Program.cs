2dbeab69b4871d1df7f262e2eb8770420c8b0c39
Rishika Jain <rishikajain22may@gmail.com>
2025-11-07 12:27:29 +0530

    Fix port binding and add root endpoint

--- Program.cs
+++ Program.cs
@@ -79,5 +79,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://*:{port}");
+app.MapGet("/", () => "Expense Tracker API is running!");
 app.Run();
