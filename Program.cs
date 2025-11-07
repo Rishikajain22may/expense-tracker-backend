2d6c440dd777335a9067e61c8c2d879dc8055d53
Rishika Jain <rishikajain22may@gmail.com>
2025-11-07 12:32:16 +0530

    Enable Swagger in Production

--- Program.cs
+++ Program.cs
@@ -68,7 +68,7 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
